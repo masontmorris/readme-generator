2dac977931aada4ab3c0b1758b98d9067a05201e
masontmorris <masonmorris8@gmail.com>
2022-04-09 05:24:49 -0400

    adds license text, implements dayjs package for copyright year

--- index.js
+++ index.js
@@ -7,8 +7,13 @@ const generateMarkdown = require("./utils/generateMarkdown.js");
 const questions = [
     {
         type: "input",
+        name: "name",
+        message: "What is your full name?",
+    },
+    {
+        type: "input",
         name: "title",
-        message: "What is the title of your project?",
+        message: "Please enter the title of your project?",
     },
     {
         type: "input",
